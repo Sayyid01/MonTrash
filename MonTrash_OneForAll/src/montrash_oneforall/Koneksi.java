366a544377d380059e9b198a7805b66e5bd36cf6
Sayyid <sayidnaufal7@gmail.com>
2019-11-20 15:10:27 +0700

    perubahan 2
    Perubahan dari 2 aplikasi berbeda untuk jenis user berbeda, menjadi satu aplikasi.
    
    Digunakan java frame yang berbeda untuk menangani user yang berbeda, mengenai tampilan yang masih default, optimisasi UI akan dilakukan setelah program bisa melakukan CRU

---
+++ MonTrash_OneForAll/src/montrash_oneforall/Koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package montrash_oneforall;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Sayyid
+ */
+public class Koneksi {
+    public static Connection conn;
+    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_NAME = "montrash_db";
+    public static final String DB_URL = "jdbc:mysql://localhost"+DB_NAME;
+    public static final String DB_UNAME = "sayyid";
+    public static final String DB_PASSWD="";
+    
+    public static Connection bukaKoneksi(){
+        if(conn == null){
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection(DB_NAME, DB_UNAME, DB_PASSWD);
+                System.out.println("Koneksi Berhasil");
+            }catch(Exception e){
+                System.err.println("Koneksi gagal"+e.getMessage());
+            }
+        }
+        return conn;
+    }
+}
--- MonTrash_OneForAll/src/montrash_oneforall/MonTrash_OneForAll.java
+++ MonTrash_OneForAll/src/montrash_oneforall/MonTrash_OneForAll.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package montrash_cleaner;
+package montrash_oneforall;
 
 /**
  *
  * @author Sayyid
  */
-public class MonTrash_Cleaner {
+public class MonTrash_OneForAll {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new formLogin().setVisible(true);
     }
     
 }
